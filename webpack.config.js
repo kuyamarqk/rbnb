dac6520e5ba54be55c3cb48f76039a484130e68a
Raymart G. Maqueda <kuyamarqk@gmail.com>
2022-10-20 14:03:53 +0800

    2nd commit

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js",
-    filename: 'another.bundle.js'
+   
   },
   module: {
     rules: [
